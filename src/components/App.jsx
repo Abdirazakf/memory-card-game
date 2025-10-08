c251ac93f289c7e74959ee289f580f9ebf73019b
Abdirazakf <farahabdirazak13@gmail.com>
2025-10-07 21:49:20 -0400

    Added header to page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,13 @@
 import '../styles/App.css'
 
 function App() {
-
+    return(
+        <>
+        <header>
+            <h1>Poker Memory Challenge</h1>
+        </header>
+        </>
+    )
 }
 
 export default App
