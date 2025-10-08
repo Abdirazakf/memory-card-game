331f0530b0e5108b911d5bb44b7d16f82d2ab6ef
Abdirazakf <farahabdirazak13@gmail.com>
2025-10-07 22:31:37 -0400

    Updated scoreboard UI

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -3,15 +3,15 @@ import '../styles/Scoreboard.css'
 export default function Scoreboard() {
     return(
         <div className="scoreboard">
-            <div className="score-container">
+            <div className="score-container">        
                 <h4>CURRENT SCORE</h4>
-                <div className="score-box current-score">
+                <div className="score-box">
                     15
                 </div>
             </div>
-            <div className=" score-container">
+            <div className="score-container">  
                 <h4>BEST SCORE</h4>
-                <div className="score-box best-score">
+                <div className="score-box">
                     22
                 </div>
             </div>
